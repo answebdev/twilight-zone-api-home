f1de84d161bb7aecceeeeb075a78cf5f10015bef
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-28 10:26:17 -0800

    fix Firefox / Safari bug

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -35,52 +35,6 @@ const Home = ({ history }) => {
     }
   }, [history.location.hash]); // Fires when component mounts and every time hash changes
 
-  (function () {
-    scrollTo();
-  })();
-
-  function scrollTo() {
-    var links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-      var link = links[i];
-      if (
-        link.href &&
-        link.href.indexOf('#') !== -1 &&
-        // eslint-disable-next-line no-restricted-globals
-        (link.pathname === location.pathname ||
-          // eslint-disable-next-line no-restricted-globals
-          '/' + link.pathname === location.pathname) &&
-        // eslint-disable-next-line no-restricted-globals
-        link.search === location.search
-      ) {
-        link.onclick = scrollAnchors;
-      }
-    }
-  }
-
-  function scrollAnchors(e, respond = null) {
-    const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
-    e.preventDefault();
-    var targetID = respond
-      ? respond.getAttribute('href')
-      : this.getAttribute('href');
-    const targetAnchor = document.querySelector(targetID);
-    if (!targetAnchor) return;
-    const originalTop = distanceToTop(targetAnchor);
-    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-    const checkIfDone = setInterval(function () {
-      const atBottom =
-        window.innerHeight + window.pageYOffset >=
-        document.body.offsetHeight - 2;
-      if (distanceToTop(targetAnchor) === 0 || atBottom) {
-        targetAnchor.tabIndex = '-1';
-        targetAnchor.focus();
-        window.history.pushState('', '', targetID);
-        clearInterval(checkIfDone);
-      }
-    }, 100);
-  }
-
   return (
     <div className={classes.PageContainer}>
       <Helmet>
