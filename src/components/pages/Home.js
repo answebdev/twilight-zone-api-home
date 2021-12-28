1d9e2aa6d24477c3248eb457edcda4dd47c60f98
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-28 10:13:18 -0800

    fix Firefox / Safari bug

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,7 +22,7 @@ const Home = ({ history }) => {
     window.scrollTo(0, 0);
   });
 
-  // Fix bug in Firefox and Safari -
+  // Fix bug in Firefox and Safari (hash links not working)-
   // Source: https://stackoverflow.com/questions/40280369/use-anchors-with-react-router
   // (scroll down to see post)
   useEffect(() => {
@@ -34,6 +34,38 @@ const Home = ({ history }) => {
         .scrollIntoView({ behavior: 'smooth' });
     }
   }, [history.location.hash]); // Fires when component mounts and every time hash changes
+
+  (function () {
+    scrollTo();
+  })();
+
+  function scrollTo() {
+    const links = document.querySelectorAll('.scroll');
+    links.forEach((each) => (each.onclick = scrollAnchors));
+  }
+
+  function scrollAnchors(e, respond = null) {
+    const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
+    e.preventDefault();
+    var targetID = respond
+      ? respond.getAttribute('href')
+      : this.getAttribute('href');
+    const targetAnchor = document.querySelector(targetID);
+    if (!targetAnchor) return;
+    const originalTop = distanceToTop(targetAnchor);
+    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+    const checkIfDone = setInterval(function () {
+      const atBottom =
+        window.innerHeight + window.pageYOffset >=
+        document.body.offsetHeight - 2;
+      if (distanceToTop(targetAnchor) === 0 || atBottom) {
+        targetAnchor.tabIndex = '-1';
+        targetAnchor.focus();
+        window.history.pushState('', '', targetID);
+        clearInterval(checkIfDone);
+      }
+    }, 100);
+  }
 
   return (
     <div className={classes.PageContainer}>
