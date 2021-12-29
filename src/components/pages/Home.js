99a6e1ddc874f298f3f0451a2c7cbd55bae3ec00
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-28 16:31:44 -0800

    fix Safari bug

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,14 +22,15 @@ const Home = ({ history }) => {
   //   // Scroll to top of page:
   //   window.scrollTo(0, 0);
   // });
-  
 
   useEffect(() => {
     Prism.highlightAll();
-    // kick off the polyfill!
-    smoothscroll.polyfill();
+
     // Scroll to top of page:
     window.scrollTo(0, 0);
+
+    // kick off the polyfill!
+    smoothscroll.polyfill();
 
     // Fix bug in Firefox and Safari (hash links not working)-
     // Source: https://stackoverflow.com/questions/40280369/use-anchors-with-react-router
