7301d48d2dcd291d557bc953a6ea8a35181b6444
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-28 16:10:43 -0800

    fix Safari bug

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,14 +22,14 @@ const Home = ({ history }) => {
   //   window.scrollTo(0, 0);
   // });
 
-  // Fix bug in Firefox and Safari (hash links not working)-
-  // Source: https://stackoverflow.com/questions/40280369/use-anchors-with-react-router
-  // (scroll down to see post)
   useEffect(() => {
     Prism.highlightAll();
     // Scroll to top of page:
     window.scrollTo(0, 0);
 
+    // Fix bug in Firefox and Safari (hash links not working)-
+    // Source: https://stackoverflow.com/questions/40280369/use-anchors-with-react-router
+    // (scroll down to see Noah's post):
     const hash = history.location.hash;
     if (hash && document.getElementById(hash.substr(1))) {
       // Check if there is a hash and if an element with that id exists
