69af6b75163e38c1fbf64e2ff85cbcfe7de6d6c1
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-28 16:17:35 -0800

    fix Safari bug

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,7 @@ import Navbar from '../../layout/Navbar';
 import Footer from '../../layout/Footer';
 import classes from '../../styles/Home.module.css';
 import useWindowDimensions from '../../utils/useWindowDimensions';
+import smoothscroll from 'smoothscroll-polyfill';
 
 import Prism from 'prismjs';
 //import 'prismjs/themes/prism-okaidia.css';
@@ -24,6 +25,8 @@ const Home = ({ history }) => {
 
   useEffect(() => {
     Prism.highlightAll();
+    // kick off the polyfill!
+    smoothscroll.polyfill();
     // Scroll to top of page:
     window.scrollTo(0, 0);
 
