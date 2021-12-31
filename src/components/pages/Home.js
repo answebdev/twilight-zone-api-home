86f40bed2229fe83d7d25c01186c875622ee00ec
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-30 19:54:00 -0800

    fix scrollToTop bug

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -44,7 +44,7 @@ const Home = ({ history }) => {
   useEffect(() => {
     // Polyfill:
     smoothscroll.polyfill();
-    //window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    window.scroll({ top: '#docs', left: 0, behavior: 'smooth' });
     // Scroll to top of page:
     window.scrollTo(0, 0);
   });
