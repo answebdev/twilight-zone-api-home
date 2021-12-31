2349857602b77b15b1a97bf3f0305e52449cc591
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-30 17:35:24 -0800

    add function to remove hash from url

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -30,12 +30,6 @@ const Home = ({ history }) => {
   });
 
   useEffect(() => {
-    window.history.replaceState(
-      '',
-      document.title,
-      window.location.origin + window.location.pathname + window.location.search
-    );
-
     Prism.highlightAll();
     // Polyfill:
     smoothscroll.polyfill();
